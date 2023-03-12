7141d315a9e7c3ccb99c0958c362d11340f7089c
Thiago Tomé <dev.tomethiago@gmail.com>
2023-03-12 12:35:36 -0300

    first commit

---
+++ src/schema.ts
@@ -0,0 +1,10 @@
+export const typeDefs = `
+  type Dinosaur {
+    name: String
+    description: String
+  }
+  type Query {
+    dinosaurs: [Dinosaur]
+    dinosaur(name: String): Dinosaur
+  }
+`;
